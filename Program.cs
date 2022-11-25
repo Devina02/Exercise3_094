b91b7edc790cd0cf22fcbcaece747513b3630c3c
Devina02 <devina.fitriapngs@gmail.com>
2022-11-25 17:28:48 +0700

    menambahkan public bool delnode

--- Program.cs
+++ Program.cs
@@ -74,7 +74,31 @@ namespace Exercise_Linked_List_A
 
 
         }
-        public bool delNode
+        public bool delNode(int rollNo)
+        {
+            Node previous, current;
+            previous = current = LAST.next;
+
+            if (Search(rollNo, ref previous, ref current) == false)
+                return false;
+            previous.next = current.next;
+
+            if (LAST.next.rollNumber == LAST.rollNumber)
+            {
+                LAST.next = null;
+                LAST = null; ;
+            }
+            else if(rollNo == LAST.next.rollNumber)
+            {
+                LAST.next = current.next;
+            }
+            else
+            {
+                LAST = LAST.next;
+            }
+            return true;
+        }
+
         public bool Search(int rollNo, ref Node previous, ref Node current)/*Searches for the specified node*/
         {
             for (previous = current = LAST.next; current != LAST; previous = current,  current = current.next)
