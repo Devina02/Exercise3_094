2eb52d6268823aa90beecd7fc0c7e075c5412615
Devina02 <devina.fitriapngs@gmail.com>
2022-11-25 17:13:38 +0700

    menghapus public  int noMhs

--- Program.cs
+++ Program.cs
@@ -9,10 +9,11 @@ namespace Exercise_Linked_List_A
     class Node
     {
         //*creates Nodes for the circular nexted list*/
-        public int noMhs;
+       
         public int rollNumber;
         public string name;
         public Node next;
+    
     }
     class Circularlist
     {
@@ -21,7 +22,10 @@ namespace Exercise_Linked_List_A
         {
             LAST = null;
         }
-        
+        public void addNote()
+        {
+
+        }
         public bool Search(int rollNo, ref Node previous, ref Node current)/*Searches for the specified node*/
         {
             for (previous = current = LAST.next; current != LAST; previous = current,  current = current.next)
